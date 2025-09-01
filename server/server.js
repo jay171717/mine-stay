20f71e5a10c7d8f516491796fa8f288f47549f2c
jay171717 <97662632+jay171717@users.noreply.github.com>
2025-09-01 14:52:20 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,16 +13,19 @@ app.use(express.static('../client'));
 io.on('connection', (socket) => {
     socket.on('startBot', (options) => botManager.startBot(options));
     socket.on('stopBot', () => botManager.stopBot());
+    socket.on('moveBot', data => botManager.moveBot(data));
+    socket.on('selectSlot', slot => {
+        if (botManager.bot && botManager.bot.inventory) botManager.bot.setQuickBarSlot(slot);
+    });
+    socket.on('swapOffhand', () => {
+        if (botManager.bot) botManager.bot.swapHands();
+    });
 
-    setInterval(() => {
+    const interval = setInterval(() => {
         socket.emit('botStatus', botManager.getStatus());
     }, 1000);
 
-    socket.on('moveBot', data => {
-        // Movement logic handled server-side with pathfinder
-    });
-    socket.on('selectSlot', slot => {});
-    socket.on('swapOffhand', () => {});
+    socket.on('disconnect', () => clearInterval(interval));
 });
 
 server.listen(3000, () => console.log('Server running on port 3000'));
